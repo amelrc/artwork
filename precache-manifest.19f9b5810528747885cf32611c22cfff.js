c3e54036b7422fd7be506f028a268bde99766b20
Amel Ramirez <amelramirez@Amels-MacBook-Air.local>
2020-06-25 17:33:27 +0200

    Updates

--- precache-manifest.19f9b5810528747885cf32611c22cfff.js
+++ precache-manifest.19f9b5810528747885cf32611c22cfff.js
@@ -1,134 +1,134 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ab50fee601831a9513d6f72839bbffc9",
-    "url": "/index.html"
+    "revision": "df0119d0cf03e30dbde8602f32558e17",
+    "url": "/artwork/index.html"
   },
   {
     "revision": "e6e42d4bcb3d8dc93f14",
-    "url": "/static/css/main.d044dea3.chunk.css"
+    "url": "/artwork/static/css/main.d044dea3.chunk.css"
   },
   {
-    "revision": "1b300572149b8dd54cb8",
-    "url": "/static/js/2.171debc2.chunk.js"
+    "revision": "889fa9b43405f0ef3572",
+    "url": "/artwork/static/js/2.fa98dd2d.chunk.js"
   },
   {
     "revision": "e6e42d4bcb3d8dc93f14",
-    "url": "/static/js/main.b291733e.chunk.js"
+    "url": "/artwork/static/js/main.b291733e.chunk.js"
   },
   {
-    "revision": "42ac5946195a7306e2a5",
-    "url": "/static/js/runtime~main.a8a9905a.js"
+    "revision": "f8eb7fc010337d20bc19",
+    "url": "/artwork/static/js/runtime~main.76b0ad81.js"
   },
   {
     "revision": "b7bea353faf10d2ffab6e7ceaef4dd5e",
-    "url": "/static/media/AC-logo.b7bea353.png"
+    "url": "/artwork/static/media/AC-logo.b7bea353.png"
   },
   {
     "revision": "f0aeb683e80446b8e8dc1cff03d2720d",
-    "url": "/static/media/c_0.f0aeb683.JPG"
+    "url": "/artwork/static/media/c_0.f0aeb683.JPG"
   },
   {
     "revision": "680581b0ac7de2aad50511e2e85b2c68",
-    "url": "/static/media/c_1.680581b0.JPG"
+    "url": "/artwork/static/media/c_1.680581b0.JPG"
   },
   {
     "revision": "2f1c6860b21743a8015aeadf20ef89f9",
-    "url": "/static/media/c_2.2f1c6860.JPG"
+    "url": "/artwork/static/media/c_2.2f1c6860.JPG"
   },
   {
     "revision": "1d2a828eebe97d478b14bb4df0b67db5",
-    "url": "/static/media/c_3.1d2a828e.JPG"
+    "url": "/artwork/static/media/c_3.1d2a828e.JPG"
   },
   {
     "revision": "d02c9e464d92d6d382f7d69fc5b56028",
-    "url": "/static/media/correo.d02c9e46.svg"
+    "url": "/artwork/static/media/correo.d02c9e46.svg"
   },
   {
     "revision": "ab02262f12aba98f8f9a8d7e571b2450",
-    "url": "/static/media/d_0.ab02262f.jpg"
+    "url": "/artwork/static/media/d_0.ab02262f.jpg"
   },
   {
     "revision": "f72959d0b4d5a61cb5da9cbde0d821b5",
-    "url": "/static/media/d_1.f72959d0.jpg"
+    "url": "/artwork/static/media/d_1.f72959d0.jpg"
   },
   {
     "revision": "02262b39c5c67558cce36d83b941690e",
-    "url": "/static/media/d_2.02262b39.jpg"
+    "url": "/artwork/static/media/d_2.02262b39.jpg"
   },
   {
     "revision": "c027b26b936354ff96885086b91c6a51",
-    "url": "/static/media/d_3.c027b26b.jpg"
+    "url": "/artwork/static/media/d_3.c027b26b.jpg"
   },
   {
     "revision": "e81d51a49012f1e3a236260fb03c144d",
-    "url": "/static/media/d_4.e81d51a4.jpg"
+    "url": "/artwork/static/media/d_4.e81d51a4.jpg"
   },
   {
     "revision": "d21c62cea63e6dd8419d2e2defac2414",
-    "url": "/static/media/i_0.d21c62ce.jpg"
+    "url": "/artwork/static/media/i_0.d21c62ce.jpg"
   },
   {
     "revision": "43f342f2d35ae949734f83e44463d280",
-    "url": "/static/media/l_0.43f342f2.jpeg"
+    "url": "/artwork/static/media/l_0.43f342f2.jpeg"
   },
   {
     "revision": "2aba4ed23c7b0a37a7ae0aa594c4d48a",
-    "url": "/static/media/l_1.2aba4ed2.jpeg"
+    "url": "/artwork/static/media/l_1.2aba4ed2.jpeg"
   },
   {
     "revision": "502a089a0b14b5c7d886b31dc270f6ff",
-    "url": "/static/media/l_2.502a089a.jpeg"
+    "url": "/artwork/static/media/l_2.502a089a.jpeg"
   },
   {
     "revision": "802578520ef57639e323f99db2fe3c8d",
-    "url": "/static/media/l_3.80257852.jpeg"
+    "url": "/artwork/static/media/l_3.80257852.jpeg"
   },
   {
     "revision": "291a2533f10485c7d25603a7b573011e",
-    "url": "/static/media/l_4.291a2533.jpg"
+    "url": "/artwork/static/media/l_4.291a2533.jpg"
   },
   {
     "revision": "1532297637b0752e50d1b6d262ba3b45",
-    "url": "/static/media/p_0.15322976.JPG"
+    "url": "/artwork/static/media/p_0.15322976.JPG"
   },
   {
     "revision": "c962f47f3367c2b35a96947fa4f26770",
-    "url": "/static/media/p_1.c962f47f.jpg"
+    "url": "/artwork/static/media/p_1.c962f47f.jpg"
   },
   {
     "revision": "19412b11134a54f8296d9a21ed34b085",
-    "url": "/static/media/p_2.19412b11.JPG"
+    "url": "/artwork/static/media/p_2.19412b11.JPG"
   },
   {
     "revision": "aa130e59a27e9c2a5a47b662a8ff6df6",
-    "url": "/static/media/p_3.aa130e59.JPG"
+    "url": "/artwork/static/media/p_3.aa130e59.JPG"
   },
   {
     "revision": "2c21e5e6085b64497e09a167ee31b331",
-    "url": "/static/media/p_4.2c21e5e6.JPG"
+    "url": "/artwork/static/media/p_4.2c21e5e6.JPG"
   },
   {
     "revision": "0ec421cfb3e0892415f43d1726258090",
-    "url": "/static/media/p_5.0ec421cf.JPG"
+    "url": "/artwork/static/media/p_5.0ec421cf.JPG"
   },
   {
     "revision": "d46e1dd0a30868e358f0e70a1380cc79",
-    "url": "/static/media/p_6.d46e1dd0.JPG"
+    "url": "/artwork/static/media/p_6.d46e1dd0.JPG"
   },
   {
     "revision": "9f8674c97ea678975b562c95fa3e26b7",
-    "url": "/static/media/p_7.9f8674c9.jpg"
+    "url": "/artwork/static/media/p_7.9f8674c9.jpg"
   },
   {
     "revision": "144cf89579dbdf723041da54ed56b1c1",
-    "url": "/static/media/s_0.144cf895.jpeg"
+    "url": "/artwork/static/media/s_0.144cf895.jpeg"
   },
   {
     "revision": "f0188a63792caf7404134774e93e1721",
-    "url": "/static/media/s_1.f0188a63.jpg"
+    "url": "/artwork/static/media/s_1.f0188a63.jpg"
   },
   {
     "revision": "385bca6866b1bc2afed7b53f3a9abe0d",
-    "url": "/static/media/s_2.385bca68.jpg"
+    "url": "/artwork/static/media/s_2.385bca68.jpg"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7683176e1c222999d49a77239a4d540d.js"
+  "/artwork/precache-manifest.19f9b5810528747885cf32611c22cfff.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/artwork/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
