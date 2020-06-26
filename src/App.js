b9b37ae389f6b0e60ecc89279205b94ba3602de0
Amel Ramirez <amelramirez@Amels-Air.localdomain>
2020-06-26 10:04:18 +0200

    [fix] - Home

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles.css';
-import { HashRouter, Route } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import { Painting } from './components/Painting';
 import { Header } from './components/header';
 import { Drawing } from './components/Drawing';
@@ -8,7 +8,7 @@ import { Cutpaper } from './components/Cutpaper';
 import { Designs } from './components/Design';
 import { Bio } from './components/bio';
 import { Footer } from './components/footer';
-// import { Home } from './components/home';
+import { Home } from './components/home';
 import styled from 'styled-components';
 import { device } from './components/art';
 
@@ -28,22 +28,15 @@ export default function App() {
 			<div className='App'>
 				<Header open={open} setOpen={setOpen} />
 				<div style={{ display: open ? 'none' : 'block' }}>
-					<Route path='/paintings' component={Painting} />
-					<Route path='/drawings' component={Drawing} />
-					<Route path='/cutpapers' component={Cutpaper} />
-					<Route path='/designs' component={Designs} />
-					<Route path='/bio' component={Bio} />
+					<Switch>
+						<Route exact path='/' component={Home} />
+						<Route path='/paintings' component={Painting} />
+						<Route path='/drawings' component={Drawing} />
+						<Route path='/cutpapers' component={Cutpaper} />
+						<Route path='/designs' component={Designs} />
+						<Route path='/bio' component={Bio} />
+					</Switch>
 					{/* <Route path='/home' component={Home} /> */}
-					<HomeText>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-						ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-						aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet,
-						consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-						labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-						exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat.
-					</HomeText>
 				</div>
 				<Footer open={open} setOpen={setOpen} />
 			</div>
